33488da695b7e521f3d63ee1480413ce1a283542
Aleksa Vukadinovic <alexvukadin8@gmail.com>
2025-08-03 17:46:51 +0200

    week 6 partial

--- 6/domaci.sql
+++ 6/domaci.sql
@@ -1,1 +1,56 @@
---TODO
+-- Za svaki smer na kome studiraju studenti pronaći studenta koji ima najviše položenih espb bodova.
+-- Izdvojiti naziv smera, indeks, ime i prezime studenta i broj položenih bodova.
+
+WITH SMER_STUDENT_BODOVI AS (SELECT SP.ID,
+                                    SP.NAZIV,
+                                    D.INDEKS,
+                                    D.IME,
+                                    D.PREZIME,
+                                    SUM(CASE WHEN I.OCENA > 5 AND I.STATUS = 'o' THEN P.ESPB ELSE 0 END) AS BODOVI
+                             FROM DA.STUDIJSKIPROGRAM SP
+                                      JOIN DA.DOSIJE D ON SP.ID = D.IDPROGRAMA
+                                      JOIN DA.ISPIT I ON D.INDEKS = I.INDEKS
+                                      JOIN DA.PREDMET P ON I.IDPREDMETA = P.ID
+                             GROUP BY SP.NAZIV, D.INDEKS, D.IME, D.PREZIME, SP.ID
+                             ORDER BY SP.NAZIV)
+SELECT S.ID,
+       S.NAZIV,
+       S.INDEKS,
+       S.IME,
+       S.PREZIME,
+       S.BODOVI
+FROM SMER_STUDENT_BODOVI S
+WHERE NOT EXISTS (SELECT *
+                  FROM SMER_STUDENT_BODOVI S2
+                  WHERE S2.ID = S.ID
+                    AND S2.BODOVI > S.BODOVI);
+
+-- Izdvojiti podatke o studentu koji je predmet, koji u nazivu na 4. i 5. poziciji sadrži nisku 'gr' i
+-- koji ima između 5 i 10 espb bodova, polagao dva puta u roku od 20 dana.
+-- Izdvojiti naziv predmeta, indeks studenta koji je polagao predmet i broj dana između ispita.
+-- Kolonu sa brojem dana između ispita nazvati 'Broj dana'.
+
+WITH STUDENT_PREDMET AS (
+    SELECT D.INDEKS, P.NAZIV
+    FROM DA.DOSIJE D JOIN DA.ISPIT I ON D.INDEKS = I.INDEKS JOIN DA.PREDMET P ON I.IDPREDMETA = P.ID
+    WHERE (P.NAZIV LIKE '___gr%' OR P.NAZIV LIKE '____gr%') AND P.ESPB BETWEEN 5 AND 10 AND I.STATUS NOT IN ('p', 'n')
+) SELECT
+      --TODO
+
+-- Izdvojiti predmet koji je polagan u samo jednom ispitnom roku. Izdvojiti naziv ispitnog roka, naziv predmeta, indeks, ime i prezime studenta koji je polagao taj predmet u tom ispitnom roku.
+-- Izdvojiti podatke o parovima studenata koji su fakultet upisali 2012, 2015. ili 2018. godine i koji su rođeni u istom mestu koje u svom nazivu sadrži podnisku 'evo' počevši od 6. pozicije. Izdvojiti indekse studenata i mesto rođenja.
+-- Izdvojiti podatke o ispitima za koje važi da je broj dobijenih bodova na ispitu 6 puta veći od broja bodova koje nosi predmet koji je polagan na ispitu. Izdvojiti indeks, ime i prezime studenta koji je polagao ispit, naziv polaganog predmeta, bodove polaganog predmeta, naziv ispitnog roka u kome je polagan ispit i dobijenu ocenu.
+-- Za svaki predmet koji je položio bar jedan student, izdvojiti naziv predmeta i indeks najboljeg studenta sa tog predmeta. Za određivanje najboljeg studenta koristiti broj bodova sa kojima je ispit položen.
+-- Za studenta koji najkraće studira fakultet izdvojiti nazive predmeta koje je položio. Ukoliko student nije položio nijedan ispit umesto naziva predmeta ispisati nisku koja sadrži karakter * onoliko puta koliko ima karaktera u prezimenu studenta.
+-- Pronaći studenta koji ima najviše položenih espb bodova. Izdvojiti indeks, ime i prezime studenta i broj položenih bodova.
+-- Izdvojiti nazive ispitnih rokova u kojima su svi predmeti iz kojih su ispite u tom roku prijavili studenti koji su fakultet upisali u novembru ili decembru položili studenti koji su fakultet upisali u septembru ili oktobru.
+-- Za svaki nivo kvalifikacije i smer sa tog nivoa izdvojiti:
+-- naziv nivoa kvalifikacija
+-- stepen studija
+-- naziv smera
+-- potreban broj položenih espb bodova da bi student diplomirao na smeru
+-- broj studenata koji su ikada upisali taj smer
+-- procenat studenata koji su diplomirali na tom smeru u odnosu na broj upisanih
+-- procenat studenata koji su se ispisali tog smera u odnosu na broj upisanih
+-- procenat studenata tog smera koji su položili bar pola espb bodova predviđenih njihovim smerom.
+-- Izdvojiti parove studenata čija imena počinju na slovo A i za koje važi da su bar tri ista predmeta položili u istom ispitnom roku
--- 6/zadaci.sql
+++ 6/zadaci.sql
@@ -1,47 +1,47 @@
--- Predmeti se kategorišu kao laki ukoliko nose manje od 6 bodova, kao teški ukoliko nose više od 8 bodova, inače su srednje teški.
--- Prebrojati koliko predmeta pripada kojoj kategoriji. Izdvojiti kategoriju i broj predmeta iz te kategorije.
+-- Predmeti se kategorišu kao laki ukoliko nose manje od 6 bodova,
+-- kao teški ukoliko nose više od 8 bodova, inače su srednje teški.
+-- Prebrojati koliko predmeta pripada kojoj kategoriji.
+-- Izdvojiti kategoriju i broj predmeta iz te kategorije.
 
--- MOJE RESENJE
-WITH PREDMETI_PO_TEZINI AS (SELECT P.ID,
-                                   P.NAZIV,
+WITH PREDMET_KATEGORIJA AS (SELECT P.NAZIV,
                                    (CASE
-                                        WHEN P.ESPB < 6 THEN 'Laki'
-                                        WHEN P.ESPB >= 6 AND P.ESPB <= 8 THEN 'Srednji'
-                                        ELSE 'Teski' END) AS TEZINA
+                                        WHEN P.ESPB < 6 THEN 'LAKI'
+                                        WHEN P.ESPB BETWEEN 6 AND 8 THEN 'SREDNJI'
+                                        ELSE 'TESKI' END) AS KATEGORIJA
                             FROM DA.PREDMET P)
-SELECT DISTINCT (SELECT COUNT(P.ID) FROM PREDMETI_PO_TEZINI P WHERE P.TEZINA = 'Laki')    AS BROJ_LAKIH,
-                (SELECT COUNT(P.ID) FROM PREDMETI_PO_TEZINI P WHERE P.TEZINA = 'Srednji') AS BROJ_SREDNJIH,
-                (SELECT COUNT(P.ID) FROM PREDMETI_PO_TEZINI P WHERE P.TEZINA = 'Teski')   AS BROJ_TESKIH
-FROM PREDMETI_PO_TEZINI P
-GROUP BY TEZINA;
+SELECT SUM(CASE WHEN PREDMET_KATEGORIJA.KATEGORIJA = 'LAKI' THEN 1 ELSE 0 END)    AS BROJ_LAKIH,
+       SUM(CASE WHEN PREDMET_KATEGORIJA.KATEGORIJA = 'SREDNJI' THEN 1 ELSE 0 END) AS SREDNJIH,
+       SUM(CASE WHEN PREDMET_KATEGORIJA.KATEGORIJA = 'TESKI' THEN 1 ELSE 0 END)   AS BROJ_TESKIH
+FROM PREDMET_KATEGORIJA;
 
--- RESENJE SA VEZBI
-WITH PREDMET_KATEGORIJA AS (SELECT ID,
-                                   CASE
-                                       WHEN ESPB < 6 THEN 'LAK'
-                                       WHEN ESPB > 8 THEN 'SREDNJE TEZAK'
-                                       ELSE 'TEZAK'
-                                       END AS TEZINA
-                            FROM DA.PREDMET)
-SELECT TEZINA, COUNT(*)
-FROM PREDMET_KATEGORIJA
-GROUP BY TEZINA;
+WITH PREDMET_KATEGORIJA AS (SELECT P.NAZIV,
+                                   (CASE
+                                        WHEN P.ESPB < 6 THEN 'LAKI'
+                                        WHEN P.ESPB BETWEEN 6 AND 8 THEN 'SREDNJI'
+                                        ELSE 'TESKI' END) AS KATEGORIJA
+                            FROM DA.PREDMET P)
+SELECT PK.KATEGORIJA, COUNT(*)
+FROM PREDMET_KATEGORIJA PK
+GROUP BY PK.KATEGORIJA;
 
 -- Izračunati koliko studenata je položilo više od 10 bodova.
-SELECT COUNT(*) AS BROJ_STUDENATA
-FROM (SELECT I.INDEKS
-      FROM DA.ISPIT I
-               JOIN DA.PREDMET P ON I.IDPREDMETA = P.ID
-      WHERE OCENA > 5
-        AND STATUS = 'o'
-      GROUP BY INDEKS
-      HAVING SUM(ESPB) > 10);
+
+WITH STUDENT_BODOVI AS (SELECT I.INDEKS, SUM(P.ESPB) AS BODOVI
+                        FROM DA.ISPIT I
+                                 JOIN DA.PREDMET P ON I.IDPREDMETA = P.ID
+                        WHERE I.OCENA > 5
+                          AND I.STATUS = 'o'
+                        GROUP BY I.INDEKS)
+SELECT COUNT(*) AS "BROJ STUDENATA"
+FROM STUDENT_BODOVI SB
+WHERE SB.BODOVI > 10;
 
 -- Naći broj ispitnih rokova u kojima su studenti položili bar 2 različita predmeta.
+
 WITH STUDENT_ROK_POLOZENO AS (SELECT I.INDEKS, I.OZNAKAROKA, I.SKGODINA, COUNT(*) AS POLOZENO
                               FROM DA.ISPIT I
-                              WHERE I.STATUS = 'o'
-                                AND OCENA > 5
+                              WHERE I.OCENA > 5
+                                AND I.STATUS = 'o'
                               GROUP BY I.INDEKS, I.OZNAKAROKA, I.SKGODINA
                               HAVING COUNT(*) > 2)
 SELECT COUNT(DISTINCT SRP.OZNAKAROKA || SRP.SKGODINA)
@@ -50,66 +50,58 @@ FROM STUDENT_ROK_POLOZENO SRP;
 -- Za svaki predmet izdvojiti identifikator i broj različitih studenata koji su ga polagali.
 -- Uz identifikatore predmeta koje niko nije polagao izdvojiti 0.
 
--- moje resenje
-WITH POLAGANI_PREDMETI AS (SELECT P.ID,
-                                  (SELECT COUNT(DISTINCT I.INDEKS)
-                                   FROM DA.ISPIT I
-                                   WHERE I.IDPREDMETA = P.ID
-                                     AND I.OCENA > 5
-                                     AND I.STATUS = 'o') AS POLAGANJA
-                           FROM DA.PREDMET P
-                                    LEFT JOIN DA.ISPIT I ON P.ID = I.IDPREDMETA)
-SELECT DISTINCT PP.ID, COALESCE(PP.POLAGANJA, 0)
-FROM POLAGANI_PREDMETI PP
-ORDER BY 2;
-
--- resenje sa vezbi
-SELECT IDPREDMETA, COUNT(DISTINCT INDEKS) "BROJ STUDENATA"
-FROM DA.ISPIT
-WHERE STATUS NOT IN ('p', 'n')
-GROUP BY IDPREDMETA
-UNION
-SELECT ID, 0
-FROM DA.PREDMET
-WHERE NOT EXISTS(SELECT *
-                 FROM DA.ISPIT I
-                 WHERE ID = IDPREDMETA
-                   AND STATUS NOT IN ('p', 'n'))
-ORDER BY 2;
+SELECT P.ID, COALESCE(COUNT(DISTINCT I.INDEKS), 0)
+FROM DA.PREDMET P
+         LEFT JOIN DA.ISPIT I ON P.ID = I.IDPREDMETA AND I.STATUS NOT IN ('p', 'n')
+GROUP BY P.ID;
 
 -- Za studenta koji ima ocenu 8 ili 9 izračunati iz koliko ispita je dobio ocenu 8 i iz koliko ispita je dobio ocenu 9.
 -- Izdvojiti indeks studenta, broj ispita iz kojih je student dobio ocenu 8 i broj ispita iz kojih je student dobio ocenu 9.
-SELECT INDEKS,
-       SUM(CASE WHEN OCENA = 8 THEN 1 ELSE 0 END) OSAM,
-       SUM(CASE WHEN OCENA = 9 THEN 1 ELSE 0 END) DEVET
-FROM DA.ISPIT
+
+SELECT I.INDEKS,
+       SUM(CASE WHEN I.OCENA = 8 THEN 1 ELSE 0 END) AS BROJ_OSMICA,
+       SUM(CASE WHEN I.OCENA = 9 THEN 1 ELSE 0 END) AS BROJ_DEVETKI
+FROM DA.ISPIT I
 WHERE OCENA = 8
    OR OCENA = 9
-GROUP BY INDEKS
-ORDER BY INDEKS;
+GROUP BY I.INDEKS;
 
--- Studentima koji su položili neki ispit, izdvojti pored imena i prezimena, naziv predmeta koji su položili iz prvog pokušaja.
-WITH INDEKS_PREDMET_POLAGANO AS (SELECT INDEKS,
-                                        NAZIV,
-                                        COUNT(*)                                                    POLAGANO,
-                                        SUM(CASE WHEN OCENA > 5 AND STATUS = 'o' THEN 1 ELSE 0 END) POLOZENO
-                                 FROM DA.ISPIT I
-                                          JOIN DA.PREDMET P
-                                               ON I.IDPREDMETA = P.ID
-                                 GROUP BY INDEKS, NAZIV, P.ID)
+-- Studentima koji su položili neki ispit, izdvojti pored imena i prezimena,
+-- naziv predmeta koji su položili iz prvog pokušaja.
+
+WITH STUNDET_PREDMET_POKUSAJI AS (SELECT D.INDEKS,
+                                         D.IME,
+                                         D.PREZIME,
+                                         P.NAZIV,
+                                         COUNT(*)                                                        AS POKUSAJI,
+                                         SUM(CASE WHEN I.OCENA > 5 AND I.STATUS = 'o' THEN 1 ELSE 0 END) AS POLOZENO
+                                  FROM DA.DOSIJE D
+                                           JOIN DA.ISPIT I ON D.INDEKS = I.INDEKS
+                                           JOIN DA.PREDMET P ON I.IDPREDMETA = P.ID
+                                  GROUP BY D.IME, D.PREZIME, P.NAZIV, D.INDEKS)
 SELECT *
-FROM DA.DOSIJE D
-         JOIN INDEKS_PREDMET_POLAGANO I
-              ON D.INDEKS = I.INDEKS
-WHERE POLAGANO = 1
+FROM STUNDET_PREDMET_POKUSAJI
+WHERE POKUSAJI = 1
   AND POLOZENO = 1;
-
 
 -- Izdvojiti ime i prezime studenta i naziv ispitnog roka u kome student ima svoj najmanji procenat uspešnosti na ispitima.
 -- Izdvojiti i procenat uspešnosti na ispitima u tom roku kao decimalan broj sa 2 cifre iza decimalne tačke.
 -- Procenat uspešnosti studenta u ispitnom roku se računa kao procenat broja položenih ispita u odnosu na broj prijavljenih ispita.
 -- Izdvojiti samo podatke za studente iz Aranđelovca i koji u tom roku imaju najmanji procenat uspešnosti u poređenju sa ostalim studentima.
--- TODO
+
+-- !!! NETACNO RESENJE TODO: FIX
+WITH STUDENT_USPESNOST AS (SELECT I.INDEKS,
+                                  IR.NAZIV,
+                                  DECIMAL(SUM(CASE WHEN I.OCENA > 5 AND I.STATUS = 'o' THEN 1.0 ELSE 0 END) /
+                                          (COUNT(*) + 0.0), 6, 2) AS USPESNOST
+                           FROM DA.ISPIT I
+                                    JOIN DA.ISPITNIROK IR ON I.OZNAKAROKA = IR.OZNAKAROKA AND I.SKGODINA = IR.SKGODINA
+                           GROUP BY I.INDEKS, IR.NAZIV)
+SELECT D.IME, D.PREZIME, SU.NAZIV, SU.USPESNOST
+FROM DA.DOSIJE D
+         JOIN STUDENT_USPESNOST SU ON D.INDEKS = SU.INDEKS
+WHERE D.MESTORODJENJA = 'Arandjelovac'
+  AND SU.USPESNOST = (SELECT MIN(SU2.USPESNOST) FROM STUDENT_USPESNOST SU2);
 
 -- Za sva imena studenata izdvojiti predmete na kojima su studenti sa tim imenom dobili najveću ocenu. Ukoliko su za neko ime studenti sa tim imenom iz više predmeta dobili maksimalnu ocenu, izdvojiti sve takve predmete. Izdvojiti ime, naziv predmeta i dobijenu ocenu. Pored toga, izdvojiti takozvani dugi kod imena. Dugi kod imena dobija se na sledeći način:
 -- ukoliko je ocena koja je izdvojena uz ime nepoznata, dugi kod jeste niska 'NULL';
